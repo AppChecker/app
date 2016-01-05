33c80e36c0be49913f069401be11da809bbc6661
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-01-05 12:18:51 -0800

    SEO-75 Replace Our404Handler extension with a simpler approach
    A simple redirect-canon.php PHP script will do the same as the whole
    extension. We'll just need to point ErrorDocument 404 to this file in
    Apache config.
    
    Unfortunately we need to parse the URL ourselves, because of the way
    ErrorDocument directive works.

---
+++ redirect-canonical.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * $_SERVER['REQUEST_URI'] holds the actual path from the URL including the leading slash
+ *
+ * We need to remove the leading slash(es). We also add a '_' at the end of it and pass it
+ * as the 'title' param to the index.php script. It recognizes it as an incorrect title
+ * and issues a redirect to the canonical one. We also pass the other GET params after
+ * parsing the URL query part.
+ *
+ * In the apache configuration just say "ErrorDocument 404 redirect-canonical.php"
+ *
+ * This simple file replaces the whole Our404Handler extension
+ */
+
+$uri = $_SERVER['REQUEST_URI'];
+$path = parse_url( $uri, PHP_URL_PATH );
+$query = parse_url( $uri, PHP_URL_QUERY );
+
+parse_str( $query, $_GET );
+$_GET['title'] = preg_replace( ':^/*:', '', $path ) . '_';
+
+require( 'index.php' );
