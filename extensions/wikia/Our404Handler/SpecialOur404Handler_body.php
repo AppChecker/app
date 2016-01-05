0be8a573a9877f16f11905827084a468475cee8b
Piotr Gabryjeluk <rychu@wikia-inc.com>
2016-01-05 12:18:51 -0800

    SEO-75 Add logging to Special:Our404Handler
    Log the URL query strings to understand what params are in use when
    redirecting.
    
    Also clean up the extension a bit with the assumption that
    $wgOur404HandlerBroaderRedirects is always true.

--- extensions/wikia/Our404Handler/SpecialOur404Handler_body.php
+++ extensions/wikia/Our404Handler/SpecialOur404Handler_body.php
@@ -43,7 +43,7 @@ class Our404HandlerPage extends UnlistedSpecialPage {
 	 * Just render some simple 404 page
 	 */
 	public function doRender404() {
-		global $wgOut, $wgContLang, $wgCanonicalNamespaceNames, $wgOur404HandlerBroaderRedirects;
+		global $wgOut, $wgContLang, $wgCanonicalNamespaceNames;
 
 		/**
 		 * check, maybe we have article with that title, if yes 301redirect to
@@ -55,54 +55,20 @@ class Our404HandlerPage extends UnlistedSpecialPage {
 		}
 		$uri = substr( parse_url( $uri, PHP_URL_PATH ), 1 );
 
-		Wikia::log( __METHOD__, false,  isset($_SERVER[ 'HTTP_REFERER' ])?$_SERVER[ 'HTTP_REFERER' ]:"[no referer]" );
 		$title = $wgContLang->ucfirst( urldecode( ltrim( $uri, "/" ) ) );
-		$namespace = NS_MAIN;
 
-		/**
-		 * first check if title is in namespace other than main
-		 */
-		$parts = explode( ":", $title, 2 );
-		if( count( $parts ) == 2 ) {
-			foreach( $wgCanonicalNamespaceNames as $id => $name ) {
-				$translated = $wgContLang->getNsText( $id );
-				if( strtolower( $translated ) === strtolower( $parts[0] ) ||
-					strtolower( $name ) === strtolower( $parts[0] ) ) {
-					$namespace = $id;
-					$title = $parts[1];
-					break;
-				}
-			}
-		}
+		$oTitle = Title::newFromText( $title );
 
-		/**
-		 * create title from parts
-		 */
-		$oTitle = Title::newFromText( $title, $namespace );
-
-		if( !is_null( $oTitle ) ) {
+		if ( !is_null( $oTitle ) ) {
 			// Preserve the query string on a redirect
-			$query = parse_url ( $_SERVER['REQUEST_URI'], PHP_URL_QUERY );
-			if( $namespace == NS_SPECIAL
-				|| $namespace == NS_MEDIA
-				|| $wgOur404HandlerBroaderRedirects
-			) {
-				/**
-				 * these namespaces are special and don't have articles
-				 */
-				header( "X-Redirected-By: Our404Handler" );
-				header( sprintf( "Location: %s", $oTitle->getFullURL($query) ), true, 301 );
-				exit( 0 );
+			$query = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_QUERY );
 
-			} else {
-				$oArticle = new Article( $oTitle );
-				if( $oArticle->exists() ) {
-					header( "X-Redirected-By: Our404Handler" );
-					header( sprintf( "Location: %s", $oArticle->getTitle()->getFullURL($query) ), true, 301 );
-					exit( 0 );
-				}
-			}
+			// Log the query string to understand what params are used
+			Wikia\Logger\WikiaLogger::instance()->debug(__METHOD__, ['qs' => $query, 'ex' => new Exception()]);
 
+			header( "X-Redirected-By: Our404Handler" );
+			header( sprintf( "Location: %s", $oTitle->getFullURL( $query ) ), true, 301 );
+			exit( 0 );
 		}
 
 		/**
@@ -111,29 +77,7 @@ class Our404HandlerPage extends UnlistedSpecialPage {
 		$wgOut->setStatusCode( 404 );
 
 		$info = wfMsgForContent( 'message404', $uri, urldecode( $title ) );
-		$wgOut->addHTML( '<h2>'.wfMsg( 'our404handler-oops' ).'</h2>
-						<div>'. $wgOut->parse( $info ) .'</div>' );
+		$wgOut->addHTML( '<h2>' . wfMsg( 'our404handler-oops' ) . '</h2>
+						<div>' . $wgOut->parse( $info ) . '</div>' );
 	}
-
-	/**
-	 * This hook is called when about to force a redirect to a canonical URL
-	 * for a title when we have no other parameters on the URL.
-	 *
-	 * Return false when we want to prevent the redirect to the canonical URL
-	 * for Our404Handler special page
-	 *
-	 * @see PLATFORM-811
-	 *
-	 * @param WebRequest $request
-	 * @param Title $title
-	 * @param OutputPage $output
-	 * @return bool
-	 */
-	public static function onTestCanonicalRedirect( WebRequest $request, Title $title, OutputPage $output) {
-		if ( $title->isSpecial( self::NAME ) ) {
-			return false;
-		}
-
-		return true;
-	}
-};
+}
