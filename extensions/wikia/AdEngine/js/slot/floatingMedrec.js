0c9b4cb5e597a9a20f55d6bd03a913d039f5764a
drets <dmitryrets@gmail.com>
2016-01-13 20:06:48 +0100

    ADEN-2712 Add unit tests

--- extensions/wikia/AdEngine/js/slot/floatingMedrec.js
+++ extensions/wikia/AdEngine/js/slot/floatingMedrec.js
@@ -35,6 +35,7 @@ define('ext.wikia.adEngine.slot.floatingMedrec', [
 		$placeHolder.append($slot);
 
 		globalNavigationHeight = $globalNavigation.height();
+
 		lastAdHeight = $slot.height();
 
 		startPosition = parseInt($placeHolder.offset().top, 10) - globalNavigationHeight - margin;
